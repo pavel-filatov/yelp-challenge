73bc09e7f2407bf7f5602958d22e98b68057d991
Pavel Filatov <filatov.py@gmail.com>
2019-07-17 17:23:15 +0300

    updated script ro run Docker and prepare an environment

--- run_docker_and_prepare_environment.sh
+++ run_docker_and_prepare_environment.sh
@@ -8,6 +8,6 @@ else
     docker rm -f $(docker ps -qa -f "name=spark-cassandra")
     docker run --name spark-cassandra -p 4040:4040 -d pfilatov/spark-cassandra
     docker cp $1 spark-cassandra:/data/yelp
-    docker exec spark-cassandra bash -c "cd /data/yelp && tar xvf $1 && rm $1"
+    docker exec spark-cassandra bash -c "cd /data/yelp && tar xvf $(ls *tar) && rm $(ls *tar)"
     docker exec -it spark-cassandra bash
 fi
